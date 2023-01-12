9919743accebf87796f054367c31893e36698816
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-01-12 22:34:46 +0100

    help meee

---
+++ main.js
@@ -0,0 +1,14 @@
+// const observer = new IntersectionObserver((entries) => {
+//     enteries.ForEach((entry) => {
+// console.log(entry)
+// if (entry.isIntersecting){
+//     entry.target.classlist.add('show');
+// } else {
+//     entry.target.classlist.remove('show');
+// }
+//     });
+// });
+
+
+// // const hiddenElements = document.querySelectorAll('.text');
+// // hiddenElements.ForEach((el) => observer.observe(el));
