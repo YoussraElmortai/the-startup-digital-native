aba0b5f6b039e40abfbb42420de1952badaa7b4a
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-01-25 10:58:13 +0100

    acces to media yeahhh

--- main.js
+++ main.js
@@ -12,3 +12,20 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".text2");
 hiddenElements.forEach((el) => observer.observe(el));
+
+// take a picture
+
+let canavs = document.querySelector("#canvas");
+let context = canvas.getContext("2d");
+let video = document.querySelector("#video");
+
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+    video.srcObject = stream;
+    video.play();
+  });
+}
+
+document.getElementById("snap").addEventListener("click", () => {
+  context.drawImage(video, 0, 0, 640, 480);
+});
