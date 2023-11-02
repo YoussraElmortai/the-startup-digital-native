73afc852aec6c997d895864522022623f891d904
youssra <youssramortai01@gmail.com>
2023-11-02 11:28:18 +0100

    ..

--- main.js
+++ main.js
@@ -14,24 +14,24 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 // take a picture
 
-let canavs = document.querySelector("#canvas");
-let context = canvas.getContext("2d");
-let video = document.querySelector("#video");
+// let canavs = document.querySelector("#canvas");
+// let context = canvas.getContext("2d");
+// let video = document.querySelector("#video");
 
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
-    video.srcObject = stream;
-    video.play();
-  });
-}
+// if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+//   navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+//     video.srcObject = stream;
+//     video.play();
+//   });
+// }
 
-document.getElementById("snap").addEventListener("click", () => {
-  context.drawImage(video, 0, 0, 640, 480);
-});
+// document.getElementById("snap").addEventListener("click", () => {
+//   context.drawImage(video, 0, 0, 640, 480);
+// });
 
-showCanvas = () => {
-  const canvas = document.getElementById("canvas");
-  canvas.dataset.hidden = false;
-};
+// showCanvas = () => {
+//   const canvas = document.getElementById("canvas");
+//   canvas.dataset.hidden = false;
+// };
 
-showCanvas();
+// showCanvas();
